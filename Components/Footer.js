f4cdd24a6f3b80956963d0dfeaa58db738aae9d9
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-10-12 15:15:37 +0100

    "Refactored Footer component and updated Home page with new layout and added image"

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,18 +1,13 @@
-import React from 'react'
+
 
 const Footer = () => {
   return (
-    <div>
-    <div id="footer" className="section-container text-darkGrayishBlue">
-    <div className="flex flex-col text-center px-5 py-6 items-center md:flex-row md:justify-between md:text-sm ">
-      <div className="order-2">
-        Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and
-        Tailwind CSS
-      </div>
+    <div className="flex flex-col text-center items-center md:flex-row md:justify-between">
       <div className="">&copy; 2024 IFZY. All Rights Reserved</div>
+      <div className="">Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and
+      Tailwind CSS</div>
     </div>
-  </div>
-    </div>
+    
   )
 }
 
