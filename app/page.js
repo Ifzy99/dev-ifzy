0426b28dd39ff44c8906137eea9ed6860f32cea7
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-10-12 15:20:21 +0100

    "Removed Footer import and replaced with custom footer content"

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 
-import Footer from "@/Components/Footer";
+
 
 export default function Home() {
   return (
     <>
-    
-      <Footer/>
+    <div className="flex flex-col text-center items-center md:flex-row md:justify-between">
+        <div className="">&copy; 2024 IFZY. All Rights Reserved</div>
+        <div className="">Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and
+        Tailwind CSS</div>
+    </div>
+   
     </>
   );
 }
