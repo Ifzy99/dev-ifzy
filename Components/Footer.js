cca0d1da6b229ea57d1f499669ed6b8539e84668
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-10-12 20:20:38 +0100

    "Removed extra space in Footer component"

--- Components/Footer.js
+++ Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="flex flex-col text-center items-center md:flex-row md:justify-between">
       <div className="">&copy; 2024 IFZY. All Rights Reserved</div>
       <div className="">Built and Designed by Olatunbosun Ifeoluwa | Powered by Next.js and
-      Tailwind CSS</div>
+          Tailwind CSS</div>
     </div>
     
   )
