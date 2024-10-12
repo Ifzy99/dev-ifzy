1c7b2231bdcc2c5808afea2f92c0cc664c3bb66c
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-10-12 14:40:48 +0100

    "Update Home component's div className from 'flex items-center justify-center min-h-screen md-flex-row md:justify-between bg-gray-100' to 'flex flex-col items-center justify-center min-h-screen bg-gray-100'"

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Footer from "@/Components/Footer";
 export default function Home() {
   return (
     <>
-    <div className="flex  items-center justify-center min-h-screen md-flex-row md:justify-between bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-4">
         Welcome to Dev Ifzy!
       </h1>
