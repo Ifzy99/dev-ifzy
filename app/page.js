9246be07efde105f4b347626fb3cd8220749e507
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-10-12 14:48:53 +0100

    "Updated styles for Home page component"

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Footer from "@/Components/Footer";
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 md:flex-row md:justify-between md:text-sm">
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-4">
         Welcome to Dev Ifzy!
       </h1>
@@ -13,8 +13,8 @@ export default function Home() {
         src={sampleImage}
         alt="Sample"
         className="rounded-lg shadow-lg"
-        width={500} // Adjust width as needed
-        height={300} // Adjust height as needed
+        width={500}
+        height={300} 
       />
       <p className="text-lg text-gray-700 mt-4">
         This is a simple Next.js application styled with Tailwind CSS.
